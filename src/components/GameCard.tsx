e40cb326cad304aa761e9720777e221236741ec8
Ye <ye@example.com>
2023-12-31 17:10:27 +0800

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Games } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconLits from "./PlatformIconLits";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Games;
@@ -12,10 +13,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-
-        <PlatformIconLits
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        ></PlatformIconLits>
+        <HStack justifyContent="space-between">
+          <PlatformIconLits
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          ></PlatformIconLits>
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
       </CardBody>
     </Card>
   );
